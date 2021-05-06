9e9681ebd1922bcc034a013362237995170fe782
giffordtompkinsiii <giffordtompkinsiii@gmail.com>
2021-05-06 15:10:40 -0700

    edits.

--- lodestar/audit.py
+++ lodestar/audit.py
@@ -1,8 +1,8 @@
 import time
+from . import logger
 from .database.maps import asset_map
 from .pipelines.daily import update_old_data
 
-from . import logger
 
 if __name__=='__main__':
     t_0 = time.time()
